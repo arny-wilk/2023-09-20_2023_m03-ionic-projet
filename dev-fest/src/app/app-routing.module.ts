802b45e5051c3dd6b98262b86bf012ea7bd2d787
arny-wilk <arnywilk@gmail.com>
2023-09-27 15:20:03 +0200

    work on navigation, home page almost finished...

--- dev-fest/src/app/app-routing.module.ts
+++ dev-fest/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {SessionsComponent} from "./pages/sessions/sessions.component";
+import {PresentateursComponent} from "./pages/presentateurs/presentateurs.component";
+import {PresentateurDetailsComponent} from "./pages/presentateur-details/presentateur-details.component";
+import {SessionDetailsComponent} from "./pages/session-details/session-details.component";
 
 const routes: Routes = [
-  {
-    path: '',
-  }
+  {path: 'sessions-details', component: SessionDetailsComponent},
+  {path: 'presentateur-details', component: PresentateurDetailsComponent},
+  {path: 'presentateurs', component: PresentateursComponent},
+  {path: 'sessions', component: SessionsComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '**', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 @NgModule({
   imports: [
--- dev-fest/src/app/app.module.ts
+++ dev-fest/src/app/app.module.ts
@@ -6,9 +6,32 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ToolbarComponent} from "./shared/toolbar/toolbar.component";
+import {NavComponent} from "./shared/nav/nav.component";
+import {ListItemsComponent} from "./shared/list-items/list-items.component";
+import {DropdownComponent} from "./shared/dropdown/dropdown.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {HeroComponent} from "./components/home-components/hero/hero.component";
+import {PresentateursComponent} from "./pages/presentateurs/presentateurs.component";
+import {SessionsComponent} from "./pages/sessions/sessions.component";
+import {NoteComponent} from "./components/note-components/note.component";
+import {PresentateurDetailsComponent} from "./pages/presentateur-details/presentateur-details.component";
+import {SessionDetailsComponent} from "./pages/session-details/session-details.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent
+    , ToolbarComponent
+    , NavComponent
+    , ListItemsComponent
+    , DropdownComponent
+    , HomeComponent
+    , PresentateursComponent
+    , SessionsComponent
+    , PresentateurDetailsComponent
+    , SessionDetailsComponent
+    , HeroComponent
+    , NoteComponent
+    , ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
